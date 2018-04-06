4fb1e2f04d4ff756dc6ad2035d0934eaf32f6b80
Dylan Collins <dylancollins@Dylans-MacBook-Pro.local>
2018-04-05 20:52:41 -0400

    Example data

--- setup.sql
+++ setup.sql
@@ -33,6 +33,27 @@ CREATE TABLE Rental (
 	date_in date
 );
 
+INSERT INTO Rental_Plan VALUES (1, 'Basic', 10, 1);
+INSERT INTO Rental_Plan VALUES (2, 'Premium', 15, 2);
+INSERT INTO Rental_Plan VALUES (3, 'Employee', 0, 9999);
+
+INSERT INTO Customers VALUES (1, 'jb1', 'password', 'Jack', 'Blanc', '885 Rubis Dr', 'Sunnyvale', 'USA', 1);
+INSERT INTO Customers VALUES (2, 'dpc', 'blueberrys', 'Dylan', 'Collins', '111 Fake St', 'Boston', 'USA', 1);
+INSERT INTO Customers VALUES (3, 'phillerj', 'best_password', 'Phil', 'Jackson', '1 Some Rd', 'Seattle', 'USA', 1);
+INSERT INTO Customers VALUES (4, 'mWhite', 'test', 'Mary', 'White', '42 Real Dr', 'San Diego', 'USA', 2);
+INSERT INTO Customers VALUES (5, 'oldschoolethel', 'secure', 'Ethel', 'Palmer', '7 Best Av', 'Ontario', 'Canada', 2);
+INSERT INTO Customers VALUES (6, 'leung.an', 'benlerner', 'Andrew', 'Leung', '1 Street Lane Circle Dr', 'Eastford', 'USA', 1);
+INSERT INTO Customers VALUES (55, 'a', 'a', 'b', 'c', '885 Rubis Dr', 'Sunnyvale', 'USA', 1);
+
+INSERT INTO Rental VALUES (1, 200, 1, '02-10-2018', '02-20-2018');
+INSERT INTO Rental VALUES (2, 100, 1, '02-10-2018', '02-20-2018');
+INSERT INTO Rental VALUES (3, 101, 1, '02-10-2018', NULL);
+INSERT INTO Rental VALUES (4, 123, 2, '02-20-2018', '02-20-2018');
+INSERT INTO Rental VALUES (5, 123, 2, '02-20-2018', '02-22-2018');
+INSERT INTO Rental VALUES (6, 124, 2, '10-10-2017', '11-1-2017');
+INSERT INTO Rental VALUES (7, 101, 3, '02-09-2018', '02-09-2018');
+INSERT INTO Rental VALUES (8, 124, 3, '02-01-2018', NULL);
+
 \copy movie from 'C:/Users/Andrew Leung/Documents/2017-2018/Spring/Database Design/imdb2015/movie.txt' with delimiter '|' null as ''
 
 
